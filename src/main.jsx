831257b3717c7d3e9e0ba30f53681e78e71e13fe
MarcGanella <marcgx8@gmail.com>
2025-12-17 20:40:29 +0100

    chore: untrack build artifacts

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,12 @@ import "./index.css";
 import App from "./App.jsx";
 import "./i18n";
 
+const base = import.meta.env.BASE_URL;
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <AuthProvider> */}
-    <BrowserRouter>
+    <BrowserRouter basename={base}>
       <App />
     </BrowserRouter>
     {/* </AuthProvider> */}
